e08f70687c904990fcf57da8d4c26138eb2f1ced
Plasnid <stuart@sackler.ca>
2022-02-02 15:25:12 -0500

    2601-1 Winter 2020 demo

---
+++ script.js
@@ -0,0 +1,38 @@
+/**
+ * * START
+ * * PROGRAM Pick up Ball
+ * * CREATE VARIABLE elbowAngle
+ * * CREATE VARIABLE fingersClosed
+ * * IF fingersClosed is true
+ * *    SET fingersClosed to false
+ * * SET elbowAngle to 90 degrees
+ * * SET fingersClosed to true
+ * * SET elbowAngle to 0 degrees
+ * * END
+ */
+
+
+
+// * let, const
+let greatFood = "Kvass and Borsht";
+let moreGreatFood = 'Pau Bhaji';
+let twoFoodsToTry = `try some ${greatFood} and ${moreGreatFood}`;
+console.log(twoFoodsToTry);
+// * how many characters are in great food?
+console.log(greatFood.length);
+
+// * boolean example
+let becomeDesigner = 1;
+let stuGrowTentacles = 0;
+
+// * floats and integers
+let myFloat = 3.14;
+let myInteger = 26;
+
+// * randomness
+Math.random();  // * gives us a number between 0 and 1
+console.log(Math.random());
+
+
+
+
